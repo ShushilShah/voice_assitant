88c343f8d2da3a4001ff09eabf0e39d612c7c61a
ShushilShah <shahshushil3@gmail.com>
2024-01-18 23:27:15 +0545

    successfully run the query and provide an awesome output from database

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 import speech_recognition as sr
 import pyttsx3
 import datetime
+from langchain_community.utilities import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
 # Set up the speech recognizer
 recognizer = sr.Recognizer()
 
 # Set up the text-to-speech engine
 engine = pyttsx3.init()
 
+load_dotenv()
+# connecting to sql database
+db = SQLDatabase.from_uri("sqlite:///content.db")
+
+# creating chain to interact with database
+llm = OpenAI(temperature=0)
+db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+
+
 # Implement voice input and processing
 
-engine.say("Hello!, My name is Talker.")
-engine.runAndWait()
+# engine.say("Hello!, My name is Talker.")
+# engine.runAndWait()
 
 
 def process_voice_input():
@@ -39,22 +52,37 @@ def process_voice_input():
 # Generate a response based on the user query
 
 
-def generate_response(query):
-    predefined_responses = {
-        "hello": "Hello! How can I assist you?",
-        "name": "My name is talker",
-        "age": "Your age is 23 years",
-        "capital": "Kathmandu",
-        "datascience": " Data science is the study of data and its behavoir",
-        "weather": "The weather today is sunny.",
-        "time": "The current time which is  {}".format(datetime.datetime.now().strftime("%H:%M")),
-        # Add more queries and responses as needed
-    }
+# def generate_response(query):
+#     predefined_responses = {
+#         "hello": "Hello! How can I assist you?",
+#         "name": "My name is talker",
+#         "age": "Your age is 23 years",
+#         "capital": "Kathmandu",
+#         "datascience": " Data science is the study of data and its behavoir",
+#         "weather": "The weather today is sunny.",
+#         "time": "The current time which is  {}".format(datetime.datetime.now().strftime("%H:%M")),
+#         # Add more queries and responses as needed
+#     }
 
-    if query.lower() in predefined_responses:
-        return predefined_responses[query.lower()]
-    else:
-        return "I'm sorry, I don't have a response for that query."
+#     if query.lower() in predefined_responses:
+#         return predefined_responses[query.lower()]
+#     else:
+#         return "I'm sorry, I don't have a response for that query."
+
+def generate_response(query):
+    result = handle_user_query(query)
+    return result
+
+# Handle user query and execute SQL queries on the database
+
+
+def handle_user_query(query):
+    try:
+        result = db_chain.run(query)
+        return result
+    except Exception as e:
+        print("Error:", str(e))
+        return "I'm sorry, there was an error processing your query."
 
 
 # Call the function to start processing voice input
