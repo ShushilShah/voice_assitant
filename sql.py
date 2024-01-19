6a95877aeaf366a42066943b18a15453b3e3c106
ShushilShah <shahshushil3@gmail.com>
2024-01-19 17:28:47 +0545

    New data are inserted

--- sql.py
+++ sql.py
@@ -14,10 +14,16 @@ Create table CONTENT(QUERY VARCHAR(255), RESPONSE VARCHAR(255));
 cursor.execute(table_info)
 
 # Insert data
-cursor.execute('''Insert into CONTENT values('name','My name is Talker')''')
+cursor.execute(
+    '''Insert into CONTENT values('hello','Hello! how can I help you?')''')
+# cursor.execute('''Insert into CONTENT values('name','My name is Talker')''')
 cursor.execute('''Insert into CONTENT values('capital','kathmandu')''')
-
-
+cursor.execute(
+    '''Insert into CONTENT values('I love you','Sorry, I am a machine. I do not have any feelings')''')
+cursor.execute(
+    '''Insert into CONTENT values('I hate you','Sorry, I am a machine. I do not have any feelings')''')
+cursor.execute(
+    '''Insert into CONTENT values('Ram','Lord Rama is a hindu religion god known as Purusotam Ram')''')
 # Displaying data
 
 print("The inserted data")
